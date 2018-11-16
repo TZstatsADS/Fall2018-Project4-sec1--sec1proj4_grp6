135c1ce1fdef9b4cf98c81abb8039b4185c2acd8
mortimerliu <43018211+mortimerliu@users.noreply.github.com>
2018-11-16 13:14:34 -0500

    Error Detection by hl3148

---
+++ lib/bigramize.R
@@ -0,0 +1,59 @@
+##############################################################
+##############################################################
+##############################################################
+
+### Authors: Hongru Liu
+### Project 4 Group 6
+
+
+bigramize_word <- function(word) {
+  results <- list()
+  n <- nchar(word)
+  k <- 0
+  list_names <- c()
+  for (i in 1:(n-1)) {
+    for (j in (i+1):n) {
+      k <- k + 1
+      results[[k]] <- paste(substring(word,i,i),substring(word,j,j),sep='')
+      list_names[k] <- paste('PD_',i,'_',j,sep='')
+    }
+  }
+  names(results) <- list_names
+  return(results)
+}
+
+bigramize_words <- function(words) {
+  results <- sapply(words,bigramize_word)
+  results <- apply(results,1,unlist)
+  return(t(results))
+}
+
+bigramize <- function(words) {
+  cat("new length \n")
+  l <- nchar(words[1])
+  if(length(words)==1) {
+    results <- bigramize_word(words)
+  } else {
+    tmp_results <- bigramize_words(words)
+    n <- dim(tmp_results)[1]
+    results <- list()
+    for (i in 1:n) {
+      results[[i]] <- tmp_results[i,]
+    }
+    list_names <- c()
+    for (i in 1:(l-1)) {
+      for (j in (i+1):l) {
+        list_names <- c(list_names, paste('PD_',i,'_',j,sep=''))
+      }
+    }
+    names(results) <- list_names
+  }
+  return(results)
+}
+
+unique_bigram <- function(l_n) {
+  return(lapply(l_n, unique))
+}
+
+
+
